54bccc7722bd419dc9b0b5453f1d1b581af2337b
Markus Achtelik <markus.achtelik@mavt.ethz.ch>
2011-11-29 17:23:51 +0100

    removed unnecessary ptam eclipse include path stuff

--- ptam/getIncludePaths.cmake
+++
@@ -1,8 +0,0 @@
-get_directory_property(inc_dirs INCLUDE_DIRECTORIES)
-
-set(filename includepaths.txt)
-file(WRITE  ${filename} "copy this to .cproject\n\n")
-
-foreach(includeLine ${inc_dirs})
-  file(APPEND ${filename} "<listOptionValue builtIn=\"false\" value=\"${includeLine}\"/>\n")
-endforeach(includeLine ${inc_dirs})
