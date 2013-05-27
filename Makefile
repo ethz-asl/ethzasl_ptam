5ae3d232f260575895a7b9018504918c535efc6e
simonlynen <simon.lynen@mavt.ethz.ch>
2013-05-27 03:06:35 +0200

    removed libcvd sources and switched to git checkout

---
+++ Makefile
@@ -0,0 +1,11 @@
+all:
+	cd ptam && make -j8
+	cd ..
+	cd ptam_com && make -j8
+	cd ..
+
+clean: 
+	cd ptam && make clean 
+	cd ..
+	cd ptam_com && make clean
+	cd ..
--- ptam/thirdparty/libcvd/installfiles/cvd_src/noarch/two_thirds_sample.cc
+++
@@ -1,7 +0,0 @@
-#include "cvd/vision.h"
-namespace CVD{
-	void twoThirdsSample(const SubImage<byte>& in, SubImage<byte>& out)
-	{
-		twoThirdsSample<byte>(in, out);
-	}
-}
